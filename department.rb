5c0de6c62604371e4d60e1f1392d337ceefc5151
Steven Cooper <stevengcooper76@gmail.com>
2016-02-17 17:49:16 -0500

    Departments can add employees.

--- department.rb
+++ department.rb
@@ -5,15 +5,15 @@ class Department < ActiveRecord::Base
   has_many :employees
 
   def department_salary
-    @staff.reduce(0.0) {|sum, e| sum + e.salary}
+    employees.reduce(0.0) {|sum, e| sum + e.salary}
   end
 
   def add_employee_review(review)
-    @review = review
+    review = review
   end
 
   def department_raise(alloted_amount)
-    raise_eligible = @staff.select {|e| yield(e)}
+    raise_eligible = staff.select {|e| yield(e)}
     amount = alloted_amount / raise_eligible.length
     raise_eligible.each {|e| e.raise_by_amount(amount)}
   end
--- employee.rb
+++ employee.rb
@@ -3,7 +3,7 @@ class Employee < ActiveRecord::Base
   belongs_to :department
 
   def add_employee_review(review)
-    @review = review
+    review = review
     positive_matches = 0
     negative_matches = 0
 
@@ -17,25 +17,25 @@ class Employee < ActiveRecord::Base
                /inconsistent/i, /inefficient/i, /(not done well)/i, /poorly/i,
                /badly/i, /rude/i, /(off topic)/i, /lack/i, /inadequate/i, /limitation/i, /(room for improvement)/i, ]
     positive.each do |r|
-      matches = @review.scan(r).count
+      matches = review.scan(r).count
       positive_matches += matches
     end
     negative.each do |r|
-      matches = @review.scan(r).count
+      matches = review.scan(r).count
       negative_matches += matches
     end
-    @satisfactory = (positive_matches > negative_matches)
+    satisfactory = (positive_matches > negative_matches)
   end
 
   def set_employee_performance(boolean)
-    @satisfactory = boolean
+    satisfactory = boolean
   end
 
   def raise_by_percent(raise_percentage)
-    @salary += (@salary * raise_percentage)
+    salary += (salary * raise_percentage)
   end
 
   def raise_by_amount(raise_amount)
-    @salary += raise_amount
+    salary += raise_amount
   end
 end
